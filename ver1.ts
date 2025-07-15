49303a15540dcabb1f89594639842973ed97aed1
Ratchapol Chantarachote <r.chantarachote@gmail.com>
2025-07-15 23:54:09 +0700

    first commit

---
+++ ver1.ts
@@ -0,0 +1,48 @@
+export default function merge(
+  collection_1: number[], // dsc sorted
+  collection_2: number[], // asc sorted
+  collection_3: number[] // asc sorted
+): number[] {
+  // asc sort
+
+  let i, j, k: number;
+
+  i = collection_1.length - 1; // last index
+  j = 0;
+  k = 0;
+
+  let res: number[] = [];
+
+  while (i >= 0 || j < collection_2.length || k < collection_3.length) {
+    // console.log(collection_1[i], collection_2[j], collection_3[k]);
+
+    const cur_min = Math.min(
+      ...[collection_1[i], collection_2[j], collection_3[k]].filter((v) => v !== undefined)
+    );
+
+    if (cur_min == collection_1[i]) {
+      res.push(collection_1[i]);
+
+      if (i >= 0) {
+        i--;
+      }
+    } else if (cur_min == collection_2[j]) {
+      res.push(collection_2[j]);
+
+      if (j < collection_2.length) {
+        j++;
+      }
+    } else {
+      res.push(collection_3[k]);
+
+      if (k < collection_3.length) {
+        k++;
+      }
+    }
+  }
+
+  return res;
+}
+
+const res = merge([9, 7, 5, 3, 2], [0, 4, 7, 8, 10, 12], [2, 3, 4, 5, 95, 150, 256]);
+console.log(res);
---
+++ ver2.ts
@@ -0,0 +1,92 @@
+function insertion_sort(arr: number[]) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = key;
+  }
+}
+
+export default function merge(
+  collection_1: number[], // dsc sorted
+  collection_2: number[], // asc sorted
+  collection_3: number[] // asc sorted
+): number[] {
+  // asc sort
+
+  let i, j, k: number;
+
+  i = collection_1.length - 1; // last index
+  j = 0;
+  k = 0;
+
+  let collection_2_and_3_pos: number[] = [];
+  let collection_2_and_3_neg: number[] = [];
+
+  while (j < collection_2.length || k < collection_3.length) {
+    if (collection_2[j] < 0) {
+      collection_2_and_3_neg.push(collection_2[j]);
+
+      j++;
+    } else if (collection_3[k] < 0) {
+      collection_2_and_3_neg.push(collection_3[k]);
+
+      k++;
+    } else if (collection_2[j] < collection_3[k]) {
+      collection_2_and_3_pos.push(collection_2[j]);
+
+      j++;
+    } else {
+      collection_2_and_3_pos.push(collection_3[k]);
+
+      k++;
+    }
+  }
+
+  insertion_sort(collection_2_and_3_neg);
+
+  j = 0;
+  k = 0;
+
+  let res: number[] = [];
+
+  while (i >= 0 || j < collection_2_and_3_pos.length || k < collection_2_and_3_neg.length) {
+    const cur_min = Math.min(
+      ...[collection_1[i], collection_2_and_3_pos[j], collection_2_and_3_neg[k]].filter((v) => v !== undefined)
+    );
+
+    if (cur_min == collection_1[i]) {
+      res.push(collection_1[i]);
+
+      if (i >= 0) {
+        i--;
+      }
+    } else if (cur_min == collection_2_and_3_pos[j]) {
+      res.push(collection_2_and_3_pos[j]);
+
+      if (j < collection_2_and_3_pos.length) {
+        j++;
+      }
+    } else {
+      res.push(collection_2_and_3_neg[k]);
+
+      if (k < collection_2_and_3_neg.length) {
+        k++;
+      }
+    }
+  }
+
+  return res;
+}
+
+const res = merge(
+  [21, 9, 7, 5, 3, 2],
+  [0, 4, -34, 7, -9, 8, 10, 12, -1],
+  [2, -74, -1, 3, 4, -45, 5, 95, 150, 256, -1]
+);
+console.log(res);
